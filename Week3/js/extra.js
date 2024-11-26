d7b25b3a8994e2d7cfc7b7677baa9f07e9226b37
auma21 <86504875+auma21@users.noreply.github.com>
2024-11-26 11:23:51 +0800

    add additional files

---
+++ Week3/js/extra.js
@@ -0,0 +1,16 @@
+// Or with jQuery
+
+// $(document).ready(function () {
+
+//     $('.collapsible').collapsible();
+
+//     $('.dropdown-trigger').dropdown();
+
+//     $('.modal').modal();
+
+// });
+
+M.AutoInit();
+
+
+
